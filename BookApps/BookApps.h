0e55ca9dd4ab4438d745625866c07dbbb8f18b4f
Ayush SIngh <ayush.singh21082@icloud.com>
2022-01-05 11:45:11 +0530

    Initial Commit

---
+++ BookApps/BookApps.h
@@ -0,0 +1,18 @@
+//
+//  BookApps.h
+//  BookApps
+//
+//  Created by Ayush Singh on 05/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BookApps.
+FOUNDATION_EXPORT double BookAppsVersionNumber;
+
+//! Project version string for BookApps.
+FOUNDATION_EXPORT const unsigned char BookAppsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BookApps/PublicHeader.h>
+
+
